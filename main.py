212026fcc884ed2de6b40fdf74b58cdeebdc50c1
Keith Emmanuel <whatwhatoh@gmail.com>
2018-11-24 23:24:28 -0800

    Video 15 finished

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import matplotlib.pyplot as plt
 from collections import namedtuple
 
 Outcome = namedtuple('Outcome', ['wagers', 'funds', 'broke', 'profit'])
-SimProperties = namedtuple('SimProperties', ['winner', 'multiple', 'bust_rate', 'profit_rate'] )
+SimProperties = namedtuple('SimProperties', ['winner', 'multiple', 'bust_rate',
+    'profit_rate', 'expected_value', 'roi'] )
 
 bust_rate_required = 31.235
 profit_rate_required = 63.208
@@ -24,26 +25,43 @@ def graph(outcomes):
 def graph_winners(winners):
     busts = []
     profits = []
+    roi = []
     multiples = []
     for each in winners:
         busts.append(each.bust_rate)
         profits.append(each.profit_rate)
+        roi.append(each.roi)
         multiples.append(each.multiple)
 
     #fig, ax = plt.subplots()
 
     plt.xlabel('Multiple Value')
     plt.ylabel('Profit Rate')
+    #plt.plot(multiples, profits, c='k', label="Profit Rate (%)")
     plt.scatter(multiples, profits, c='k', label="Profit Rate (%)")
     plt.show()
 
     plt.xlabel('Multiple Value')
     plt.ylabel('Bust Rate')
+    #plt.plot(multiples, busts, c='c', label="Bust Rate(%)")
     plt.scatter(multiples, busts, c='c', label="Bust Rate(%)")
+    plt.show()
+
+    plt.xlabel('Multiple Value')
+    plt.ylabel('ROI Rate')
+    #plt.plot(multiples, busts, c='c', label="Bust Rate(%)")
+    plt.scatter(multiples, roi, c='c', label="ROI(%)")
     plt.show()
 
 
 def rollDice():
+    '''True player wins, False player loses'''
+    roll = random.randint(1, 100)
+    if roll <= 50:
+        return False
+    return True
+
+def rollUnfairDice():
     '''True player wins, False player loses'''
     roll = random.randint(1, 100)
     if roll == 100:
@@ -52,9 +70,9 @@ def rollDice():
         return False
     elif 100 > roll > 50:
         return True
-
 def multiple_bettor(funds, initial_wager, wager_count, multiple=0):
     global random_mulitple
+    gamble = rollUnfairDice
     value = funds
     wager = initial_wager
     num_wagers = 1
@@ -66,9 +84,9 @@ def multiple_bettor(funds, initial_wager, wager_count, multiple=0):
         multiple = random_mulitple
 
     while num_wagers <= wager_count:
-        if DEBUG > 100:
+        if DEBUG >= 200:
             print("On wager: ", num_wagers, end="\r")
-        if rollDice():
+        if gamble():
             value += wager
             wager = initial_wager
 
@@ -93,6 +111,7 @@ def multiple_bettor(funds, initial_wager, wager_count, multiple=0):
 
 def dalembert_bettor(funds, initial_wager, wager_count, multiple=0):
     global random_mulitple
+    gamble = rollDice
     value = funds
     wager = initial_wager
     num_wagers = 1
@@ -102,9 +121,10 @@ def dalembert_bettor(funds, initial_wager, wager_count, multiple=0):
     vY = []
 
     while num_wagers <= wager_count:
-        if DEBUG > 100:
-            print("On wager: ", num_wagers, end="\r")
-        if rollDice():
+        if DEBUG >= 200:
+            print("On wager: ", num_wagers)
+            print("Current wager: $", wager)
+        if gamble():
             value += wager
             if wager <= initial_wager:
                 wager = initial_wager
@@ -149,48 +169,59 @@ def simulation(bettor):
     profit_count = 0
     x = 0
 
+    total_winnings = 0.0
+
     while x < sample_size:
         outcome = bettor(starting_funds, initial_wager, max_wagers)
         if outcome.broke:
             broke_count += 1
         if outcome.profit:
             profit_count += 1
+
+        if len(outcome.funds) > 0:
+            total_winnings += outcome.funds[-1]
+
         outcomes.append(outcome)
         x += 1
 
-    calc_bust_rate = broke_count*100/float(sample_size)
-    calc_profit_rate = profit_count*100/float(sample_size)
-
-    if DEBUG > 100:
+    total_invested      = starting_funds * sample_size
+    calc_bust_rate      = broke_count*100/float(sample_size)
+    calc_profit_rate    = profit_count*100/float(sample_size)
+    calc_expected_value = 1.00 * total_winnings / total_invested
+    calc_roi            = (total_winnings - total_invested) * 100.00 / total_invested
+    if DEBUG >= 100:
         print("\ndeath rate: ", calc_bust_rate)
         print("survival rate:", 100 - calc_bust_rate)
         print("Profitability %: ", calc_profit_rate)
+        print("Expected Value: ", calc_expected_value)
+        print("total_winnings: ", total_winnings)
+        print("total_invested: ", total_invested)
+        print("ROI: ", calc_roi, "%")
 
     if (calc_bust_rate < bust_rate_required) and \
         (calc_profit_rate > profit_rate_required):
         winner = True
-        if DEBUG > 10:
+        if DEBUG >= 10:
             print("\nWINNER ##############")
             print("Random Multiple", random_mulitple)
             print("Lower bust to beat", bust_rate_required)
             print("higher profit rate to beat", profit_rate_required)
             print("bust rate", calc_bust_rate)
             print("profit rate", calc_profit_rate)
-    # else:
-    #     print("\Loser :( ##############")
-    #     print("Random Multiple", random_mulitple)
-    #     print("Lower bust to beat", bust_rate_required)
-    #     print("higher profit rate to beat", profit_rate_required)
-    #     print("bust rate", calc_bust_rate)
-    #     print("profit rate", calc_profit_rate)
+            print("Expected value per dollar: ", calc_expected_value)
+            print("total_winnings: ", total_winnings)
+            print("total_invested: ", total_invested)
+            print("ROI: ", calc_roi, "%")
+
 
     if graphing:
         graph(outcomes)
 
-    return outcomes, SimProperties(winner, random_mulitple, calc_bust_rate, calc_profit_rate)
+    return outcomes, SimProperties(winner, random_mulitple, calc_bust_rate,
+        calc_profit_rate, calc_expected_value, calc_roi)
 
-def random_simulation():
-    '''Run a large number of random simulations
+def random_mulitple_simulation():
+    '''Run a large number of simulations with random mulitple values
     and return a sorted list of results by success score'''
     global random_mulitple
     max_cycles  = 1000
@@ -209,30 +240,37 @@ def random_simulation():
     num_top_winners = min(5, len(winners))
     print("#Top 5 Winners sorted by Profit Rate#")
     for x in range(0, num_top_winners):
-        print("#########")
-        print("Multiple: ", by_profit_rate[x].multiple)
-        print("Profit Rate: ", by_profit_rate[x].profit_rate)
-        print("Bust Rate: ", by_profit_rate[x].bust_rate)
+        print_winner(by_profit_rate[x])
 
     print("#Top 5 Winners sorted by Bust Rate#")
     by_bust_rate = sorted(winners, key=lambda x: x.bust_rate)
     for x in range(0, num_top_winners):
-        print("#########")
-        print("Multiple: ", by_bust_rate[x].multiple)
-        print("Profit Rate: ", by_bust_rate[x].profit_rate)
-        print("Bust Rate: ", by_bust_rate[x].bust_rate)
+        print_winner(by_bust_rate[x])
+
+    print("#Top 5 Winners sorted by ROI#")
+    by_roi = sorted(winners, key=lambda x: x.roi, reverse = True)
+    for x in range(0, num_top_winners):
+        print_winner(by_roi[x])
 
     graph_winners(winners)
 
+def print_winner(winner):
+        print("#########")
+        print("Multiple: ", winner.multiple)
+        print("Profit Rate: ", winner.profit_rate)
+        print("Bust Rate: ", winner.bust_rate)
+        print("ROI: ", winner.roi)
+        print("Expected Value: ", winner.expected_value)
+        print()
 
 def main():
     global graphing, DEBUG
-    graphing = True
+    graphing = False
     DEBUG = 0
     # simulation(simple_bettor)
     # simulation(doubler_bettor)
-    # random_simulation()
-    simulation(dalembert_bettor)
+    random_mulitple_simulation()
+    # simulation(dalembert_bettor)
 
 
     if graphing:
